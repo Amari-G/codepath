9ed0322537b68f614f8c542132935d6437fb74a1
Amari Garrett <amarig@fb.com>
2020-06-29 18:23:30 -0400

    - Fetch home timeline using TwitterClient
    - Deserialize tweet data
    
    - Display tweet in the TimelineActivity via a RecyclerView

--- Projects/Porygon/app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ Projects/Porygon/app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,14 +1,70 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
+
+    private static final String TAG = "TimelineActivity";
+
+    TwitterClient mClient;
+    RecyclerView mRecyclerViewTweets;
+    List<Tweet> mTweets;
+    TweetsAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        mClient = TwitterApp.getRestClient(this);
+
+        // Find the recycler view
+        mRecyclerViewTweets = findViewById(R.id.rvTweets);
+        // Initialize the list of tweets and adapter
+        mTweets = new ArrayList<>();
+        mAdapter = new TweetsAdapter(this, mTweets);
+        // RecyclerView setup: layout manager and the adapter
+        mRecyclerViewTweets.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerViewTweets.setAdapter(mAdapter);
+
+        populateHomeTimeline();
+    }
+
+    private void populateHomeTimeline() {
+        mClient.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "onSuccess!" + json.toString());
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    mTweets.addAll(Tweet.fromJsonArray(jsonArray));
+                    mAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    Log.e(TAG, "JSON exception", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "onFailure:" + response, throwable);
+            }
+        });
     }
 }
