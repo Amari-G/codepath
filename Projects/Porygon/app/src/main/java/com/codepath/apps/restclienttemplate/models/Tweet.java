c16d83a3e87c2807b1aef28eed5dba3ad8dca3b6
Amari Garrett <amarig@fb.com>
2020-06-30 10:04:39 -0400

    - Implement infinite scroll

--- Projects/Porygon/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ Projects/Porygon/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class Tweet {
 
+    public long id;
     public String body;
     public String createdAt;
     public User user;
@@ -16,6 +17,7 @@ public class Tweet {
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         // Parse data from JSONObject
         Tweet tweet = new Tweet();
+        tweet.id = jsonObject.getLong("id");
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
